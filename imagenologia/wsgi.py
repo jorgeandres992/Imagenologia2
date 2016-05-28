b9bc8f7f340fe0a462d2ad6524fad9262221ffc1
jorgeandres992 <jorge-patino@upc.edu.co>
2016-05-28 13:36:38 -0500

    Update wsgi.py
    nn

--- imagenologia/wsgi.py
+++ imagenologia/wsgi.py
@@ -5,7 +5,6 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
 
 import os
 
@@ -13,4 +12,30 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "imagenologia.settings")
 
+application = get_wsgi_application()
+
+"""
+
+import os, sys
+
+sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR']))
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'imagenologia.settings'
+
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
+
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
